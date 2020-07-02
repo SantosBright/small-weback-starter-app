55d7537ed1c3fff8ee962ce2eca2e1f11992369d
Santos Bright <itzsantosbright15@gmail.com>
2020-07-02 17:44:40 +0100

    add webpack config file

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,247 @@
-!function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}([function(e,t,r){"use strict";r.r(t);const n=(...e)=>e.every(e=>"number"==typeof e&&!isNaN(e));const o=new class{constructor(){this.errorBox=document.getElementById("error")}handleAdditionError(e,t){const r=e.reduce((e,r,o)=>n(t[o])?e+"":e+`${r} is not a number`,"Please enter two valid numbers!");this.errorBox.classList.remove("invisible"),this.errorBox.innerText=r}hideErrors(){this.errorBox.classList.add("invisible")}},u=new class{constructor(){this.numberOneInput=document.getElementById("numberOne"),this.numberTwoInput=document.getElementById("numberTwo"),this.addValuesButton=document.getElementById("addValues"),this.resultDiv=document.getElementById("result")}getInputs(){return[this.numberOneInput.value,this.numberTwoInput.value]}setResult(e){this.resultDiv.innerText=e}onClick(e){this.addValuesButton.addEventListener("click",e)}};var s,i;i=u,(s=o).hideErrors(),i.onClick(()=>{s.hideErrors();const e=i.getInputs(),t=((...e)=>e.map(e=>parseInt(e)))(...e);if(n(...t)){const[e,r]=t;i.setResult(e+r)}else i.setResult(""),s.handleAdditionError(e,t)})}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/app/alert.service.js":
+/*!**********************************!*\
+  !*** ./src/app/alert.service.js ***!
+  \**********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return AlertService; });
+/* harmony import */ var _utils_inputs_are_valid__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils/inputs-are-valid */ "./src/app/utils/inputs-are-valid.js");
+
+
+class AlertService{
+    constructor(){
+        this.errorBox = document.getElementById('error');
+    }
+    handleAdditionError(inputs, numbers){
+        const fullMessage = inputs.reduce((message, str, index) => {
+            if(Object(_utils_inputs_are_valid__WEBPACK_IMPORTED_MODULE_0__["inputsAreValid"])(numbers[index])){
+                return message + "";
+            } else {
+                return message + `${str} is not a number`;
+            }
+        }, "Please enter two valid numbers!");
+
+        this.errorBox.classList.remove("invisible");
+        this.errorBox.innerText = fullMessage;
+    }
+
+    hideErrors(){
+        this.errorBox.classList.add("invisible");
+    }
+}
+
+/***/ }),
+
+/***/ "./src/app/app.js":
+/*!************************!*\
+  !*** ./src/app/app.js ***!
+  \************************/
+/*! exports provided: run */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "run", function() { return run; });
+/* harmony import */ var _utils_inputs_are_valid__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils/inputs-are-valid */ "./src/app/utils/inputs-are-valid.js");
+/* harmony import */ var _utils_parse_inputs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./utils/parse-inputs */ "./src/app/utils/parse-inputs.js");
+
+
+
+const run = (alertService, componentService) => {
+  alertService.hideErrors();
+
+  componentService.onClick(() => {
+    alertService.hideErrors();
+    const inputs = componentService.getInputs();
+    const parsedInputs = Object(_utils_parse_inputs__WEBPACK_IMPORTED_MODULE_1__["parseInputs"])(...inputs);
+    if(Object(_utils_inputs_are_valid__WEBPACK_IMPORTED_MODULE_0__["inputsAreValid"])(...parsedInputs)) {
+      const [numA, numB] = parsedInputs;
+      componentService.setResult(numA + numB);
+    } else {
+      componentService.setResult("");
+      alertService.handleAdditionError(inputs, parsedInputs);
+    }
+  });
+}
+
+/***/ }),
+
+/***/ "./src/app/component.service.js":
+/*!**************************************!*\
+  !*** ./src/app/component.service.js ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return ComponentService; });
+class ComponentService{
+    constructor(){
+        this.numberOneInput = document.getElementById('numberOne');
+        this.numberTwoInput = document.getElementById('numberTwo');
+        this.addValuesButton = document.getElementById('addValues');
+        this.resultDiv = document.getElementById('result');
+    }
+
+    getInputs() {
+        return [this.numberOneInput.value, this.numberTwoInput.value];
+    }
+
+    setResult(str){
+        this.resultDiv.innerText = str;
+    }
+
+    onClick(cb) {
+        this.addValuesButton.addEventListener("click", cb);
+    }
+}
+
+/***/ }),
+
+/***/ "./src/app/utils/inputs-are-valid.js":
+/*!*******************************************!*\
+  !*** ./src/app/utils/inputs-are-valid.js ***!
+  \*******************************************/
+/*! exports provided: inputsAreValid */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "inputsAreValid", function() { return inputsAreValid; });
+const inputsAreValid = (...input) => {
+    return input.every(num => typeof num === "number" && !isNaN(num));
+};
+
+/***/ }),
+
+/***/ "./src/app/utils/parse-inputs.js":
+/*!***************************************!*\
+  !*** ./src/app/utils/parse-inputs.js ***!
+  \***************************************/
+/*! exports provided: parseInputs */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "parseInputs", function() { return parseInputs; });
+const parseInputs = (...input) => {
+    return input.map(str => parseInt(str));
+};
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _app_app__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./app/app */ "./src/app/app.js");
+/* harmony import */ var _app_alert_service__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./app/alert.service */ "./src/app/alert.service.js");
+/* harmony import */ var _app_component_service__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/component.service */ "./src/app/component.service.js");
+
+
+
+
+const alertService = new _app_alert_service__WEBPACK_IMPORTED_MODULE_1__["default"]();
+const componentService = new _app_component_service__WEBPACK_IMPORTED_MODULE_2__["default"]();
+Object(_app_app__WEBPACK_IMPORTED_MODULE_0__["run"])(alertService, componentService);
+
+/***/ })
+
+/******/ });
