eb69bd46276ab579c9a6a7b9813cc8e37783b912
Santos Bright <itzsantosbright15@gmail.com>
2020-07-03 13:12:24 +0100

    extract and minify css into seperate files, minify html and js all in production

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,10 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: {
         main: './src/index.js',
         vendor: './src/vendor.js'
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/template.html'
-    })],
     module: {
         rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader",  // 3. Injects styles/css into dom
-                    "css-loader",    // 2. Turns css into commonjs
-                    "sass-loader"    // 1. turn sass into css
-                ]
-            },
             {
                 test: /\.html$/,
                 use: ["html-loader"]
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const common = require('./webpack.common');
-var merge = require('webpack-merge');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
@@ -9,5 +13,31 @@ module.exports = merge(common, {
         filename: '[name].[contentHash].bundle.js',
         path: path.resolve(__dirname, "dist")
     },
-    plugins: [new CleanWebpackPlugin()]
+    optimization: {
+        minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+                removeComments: true
+            }
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,  // 3. Extract css into files
+                    "css-loader",    // 2. Turns css into commonjs
+                    "sass-loader"    // 1. turn sass into css
+                ]
+            }
+        ]
+    }
 });
