ea239b6a9f8aa25acad66992d9e4003c050e7ed5
Santos Bright <itzsantosbright15@gmail.com>
2020-07-03 02:30:15 +0100

    add production and development config files

---
+++ webpack.dev.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const common = require('./webpack.common');
+var merge = require('webpack-merge');
+module.exports = merge(common, {
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, "dist")
+    }
+});
